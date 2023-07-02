0690c0c5afecff11b914c82f62cdf618c9a29b29
2bo <rheinbund@gmail.com>
2023-07-02 23:05:43 +0900

    リポジトリの移動

---
+++ public/fizzbuzz.py
@@ -0,0 +1,17 @@
+def fizzbuzz(num):
+    if  num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
+
+
+if __name__ == '__main__':
+    main()
